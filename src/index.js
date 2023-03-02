5f2029f06f47dc9c1415c40b1f3173f1a68441b7
divyasonara7 <divyasonara7105@gmail.com>
2023-03-02 15:38:09 +0530

    feat: configured firebase database

--- src/index.js
+++ src/index.js
@@ -1,27 +1,57 @@
+/* 
+dependances 
+*/
 const express = require('express')
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+/* 
+Config - express 
+*/
+
 const app = express()
 const port = 3000
 
-app.get('/', (request, response) => {
 
+/* 
+Config- firebase
+*/
+const serviceAccount = require('../serviceAccountKey.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+
+
+
+/* 
+Config- posts
+*/
+
+app.get('/', (request, response) => {
   response.send("welcom to quasagram!")
 })
 
-
 app.get('/posts', (request, response) => {
-  let posts = [
-    {
-      caption: 'Golden gate Bridge',
-      location: 'Rajkot',
-    },
-    {
-      caption: 'River front',
-      location: 'Ahemdabad',
-    }
-  ];
-  response.send(posts)
+  let posts = [];
+db.collection('posts').get().then( snapshot => {
+    snapshot.forEach((doc) => {
+      console.log("document",doc.data());
+      posts.push(doc.data());
+      });
+      response.send(posts)
+    });
 })
 
-app.listen(port, () => {
+
+
+
+/* 
+Config- listen
+*/
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
 })
